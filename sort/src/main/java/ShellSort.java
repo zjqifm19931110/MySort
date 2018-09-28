0f0c27e3b3f9d93366c1cfea4e971faca6410453
bjzhaojianqiang <bjzhaojianqiang@corp.netease.com>
2018-09-28 17:23:19 +0800

    希尔排序

--- sort/src/main/java/ShellSort.java
+++ sort/src/main/java/ShellSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by bjzhaojianqiang on 2018/9/28.
  * 参考：https://blog.csdn.net/www_wangjun/article/details/73291502
@@ -9,5 +11,26 @@ public class ShellSort {
         if(arr == null || arr.length<=1){
             return;
         }
+
+        int tmp;
+        int gap = arr.length/2;
+        while(gap>0){
+            for(int i=gap; i<arr.length; i++){
+                int j = i - gap;
+                while(j>=0 && arr[j]>arr[j+gap]){
+                    tmp = arr[j];
+                    arr[j] = arr[j+gap];
+                    arr[j+gap] = tmp;
+                    j = j - gap;
+                }
+            }
+            gap = gap/2;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10,9,8,7,6,5,4,3,1};
+        shellSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
